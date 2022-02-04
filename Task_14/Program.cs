ee12fb37c1ab4a609747a9b5a47f8c32e05e8e38
Konstantin <constantin8271@gmail.com>
2022-02-04 16:38:35 +0300

    task_14

---
+++ Task_14/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти третью цифру числа или сообщить, что её нет.
+
+Console.Write("Insert the number: ");
+
+int num = int.Parse(Console.ReadLine());
+int Count = 0;
+int numbercount = num;
+while(numbercount != 0)
+{
+    numbercount /= 10;
+    Count++;
+}
+if(Count >=3)
+{
+    int number = (num /((int)Math.Pow(10,(-1*(3 - Count))))) % 10;
+    Console.WriteLine("at the number" + num + "the third digit is" + number + ".");
+}
+else
+{
+    Console.WriteLine("at the number" + num + "no third digit.");
+}
