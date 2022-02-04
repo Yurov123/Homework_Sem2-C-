b21044751cf3c1717e4e08659a4b7685d17dacf5
Konstantin <constantin8271@gmail.com>
2022-02-04 17:02:03 +0300

    task_16

---
+++ Task_16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.Write("Enter number: ");
+int num = Convert.ToInt16(Console.ReadLine());
+switch (num)
+{
+    case 1: Console.WriteLine("Monday"); break;
+    case 2: Console.WriteLine("Tuesday"); break;
+    case 3: Console.WriteLine("Wednesday"); break;
+    case 4: Console.WriteLine("Thursday"); break;
+    case 5: Console.WriteLine("Friday"); break;
+    case 6: Console.WriteLine("Saturday day off"); break;
+    case 7: Console.WriteLine("Sunday day off"); break;
+    default: Console.WriteLine("ERROR"); break;
+}    
